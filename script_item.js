dbcd362fceac5ec137e450f50707331df4283296
TitorPs360 <TitorPs360Gamer@gmail.com>
2023-03-22 11:51:53 +0700

    feat: Update items

--- script_item.js
+++ script_item.js
@@ -1,23 +1,22 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = '127.0.0.1:3000';
 
 let itemsData;
 
 // TODO #2.1: Edit group number
 const getGroupNumber = () => {
-  return 99;
+  return 7;
 };
 
 // TODO #2.2: Show group members
 const showGroupMembers = async () => {
-  const member_list = document.getElementById("member-list");
-  member_list.innerHTML = "";
-  const member_dropdown = document.getElementById("name-to-add");
-  member_dropdown.innerHTML =
-    "<option value='0'>-- เลือกผู้ฝากซื้อ --</option>";
+  const member_list = document.getElementById('member-list');
+  member_list.innerHTML = '';
+  const member_dropdown = document.getElementById('name-to-add');
+  member_dropdown.innerHTML = "<option value='0'>-- เลือกผู้ฝากซื้อ --</option>";
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
   await fetch(`http://${backendIPAddress}/items/members`, options)
     .then((response) => response.json())
@@ -28,7 +27,7 @@ const showGroupMembers = async () => {
           <li>${member.full_name}</li>
           `;
         // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
-        member_dropdown.innerHTML += ``;
+        member_dropdown.innerHTML += `<option value='${member.full_name}'>${member.full_name}</option>`;
         // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
       });
     })
@@ -37,25 +36,42 @@ const showGroupMembers = async () => {
 
 // TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
 const getItemsFromDB = async () => {
-  console.log(
-    "This function should fetch 'get items' route from backend server."
+  var myHeaders = new Headers();
+  myHeaders.append(
+    'Cookie',
+    'connect.sid=s%3AWo7NyvK85A-DyGLW9GgedlNPNpVdeQeO.NFieR5likbFQssSo4ioe%2FuWgnXmIVa2PzRAwzEOylIk'
   );
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+    credentials: 'include',
+  };
+
+  itemsData = await fetch(`http://${backendIPAddress}/items`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => JSON.parse(result))
+    .catch((error) => {
+      console.log('error', error);
+      return [];
+    });
 };
 
 // TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
 const showItemsInTable = (itemsData) => {
-  const table_body = document.getElementById("main-table-body");
-  table_body.innerHTML = "";
+  const table_body = document.getElementById('main-table-body');
+  table_body.innerHTML = '';
   // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
-
+  itemsData.sort((a, b) => a.created_date - b.created_date);
   // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
   itemsData.map((item) => {
     // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
     table_body.innerHTML += `
         <tr id="${item.item_id}">
             <td>${item.item}</td>
-            <td>Name</td>
-            <td>Price</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
             <td><button class="delete-row" onclick="deleteItem('${item.item_id}')">ลบ</button></td>
         </tr>
         `;
@@ -65,40 +81,74 @@ const showItemsInTable = (itemsData) => {
 
 // TODO #2.5: Send Add an item ("POST") request to backend server and update items in the table
 const addItem = async () => {
-  const item = document.getElementById("item-to-add").value;
-  const name = document.getElementById("name-to-add").value;
-  const price = document.getElementById("price-to-add").value;
+  const item = document.getElementById('item-to-add').value;
+  const name = document.getElementById('name-to-add').value;
+  const price = document.getElementById('price-to-add').value;
 
-  console.log(
-    "This function should fetch 'add item' route from backend server and update items in the table."
+  var myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append(
+    'Cookie',
+    'connect.sid=s%3A-mVRjXMtN3AJuD1nE1JkKd97KiqO_DMd.AGtJB6SDtk07rByt9n%2Fp0z3wtTrf%2FZ5O%2BD7iGVD%2FKEA'
   );
+
+  var raw = JSON.stringify({
+    item: item,
+    name: name,
+    price: parseInt(price),
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+    credentials: 'include',
+  };
+
+  fetch(`http://${backendIPAddress}/items`, requestOptions)
+    .then(() => redrawDOM())
+    .catch((error) => console.log('error', error));
 };
 
 // TODO 2.6: Send Delete an item ("DELETE") request to backend server and update items in the table
 const deleteItem = async (item_id) => {
-  console.log(
-    "This function should fetch 'delete item' route in backend server and update items in the table."
+  var myHeaders = new Headers();
+  myHeaders.append(
+    'Cookie',
+    'connect.sid=s%3A-mVRjXMtN3AJuD1nE1JkKd97KiqO_DMd.AGtJB6SDtk07rByt9n%2Fp0z3wtTrf%2FZ5O%2BD7iGVD%2FKEA'
   );
+
+  var requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    redirect: 'follow',
+    credentials: 'include',
+  };
+
+  fetch(`http://${backendIPAddress}/items/${item_id}`, requestOptions)
+    .then(() => redrawDOM())
+    .catch((error) => console.log('error', error));
 };
 
 const redrawDOM = () => {
   window.document.dispatchEvent(
-    new Event("DOMContentLoaded", {
+    new Event('DOMContentLoaded', {
       bubbles: true,
       cancelable: true,
     })
   );
-  document.getElementById("item-to-add").value = "";
-  document.getElementById("name-to-add").value = "0";
-  document.getElementById("price-to-add").value = "";
+  document.getElementById('item-to-add').value = '';
+  document.getElementById('name-to-add').value = '0';
+  document.getElementById('price-to-add').value = '';
 };
 
-document.getElementById("group-no").innerHTML = getGroupNumber();
+document.getElementById('group-no').innerHTML = getGroupNumber();
 
-document.addEventListener("DOMContentLoaded", async function (event) {
-  console.log("Showing group members.");
+document.addEventListener('DOMContentLoaded', async function (event) {
+  console.log('Showing group members.');
   await showGroupMembers();
-  console.log("Showing items from database.");
+  console.log('Showing items from database.');
   await getItemsFromDB();
   showItemsInTable(itemsData);
 });
