9b05344db353422fc8e942ed5b9204e9c2e3ca87
TitorPs360 <TitorPs360Gamer@gmail.com>
2023-03-22 12:11:42 +0700

    feat: Update backendIPAddress

--- script_item.js
+++ script_item.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = '127.0.0.1:3000';
+const backendIPAddress = 'http://54.221.188.147:3000';
 
 let itemsData;
 
