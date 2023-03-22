cc22e77b635074198d6e79c152b8a8cda7690599
TitorPs360 <TitorPs360Gamer@gmail.com>
2023-03-22 11:51:59 +0700

    feat: Update CV

--- script_cv.js
+++ script_cv.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = '127.0.0.1:3000';
 
 const authorizeApplication = () => {
   window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
@@ -7,27 +7,24 @@ const authorizeApplication = () => {
 
 // TODO #3.1: Change group number
 const getGroupNumber = () => {
-  return 99;
+  return 7;
 };
 
 // Example: Send Get user profile ("GET") request to backend server and show the response on the webpage
 const getUserProfile = async () => {
   const options = {
-    method: "GET",
-    credentials: "include",
+    method: 'GET',
+    credentials: 'include',
   };
-  await fetch(
-    `http://${backendIPAddress}/courseville/get_profile_info`,
-    options
-  )
+  await fetch(`http://${backendIPAddress}/courseville/get_profile_info`, options)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.user);
       document.getElementById(
-        "eng-name-info"
+        'eng-name-info'
       ).innerHTML = `${data.user.title_en} ${data.user.firstname_en} ${data.user.lastname_en}`;
       document.getElementById(
-        "thai-name-info"
+        'thai-name-info'
       ).innerHTML = `${data.user.title_th} ${data.user.firstname_th} ${data.user.lastname_th}`;
     })
     .catch((error) => console.error(error));
@@ -36,26 +33,46 @@ const getUserProfile = async () => {
 // TODO #3.3: Send Get Courses ("GET") request to backend server and filter the response to get Comp Eng Ess CV_cid
 //            and display the result on the webpage
 const getCompEngEssCid = async () => {
-  document.getElementById("ces-cid-value").innerHTML = "";
-  console.log(
-    "This function should fetch 'get courses' route from backend server and find cv_cid value of Comp Eng Ess."
-  );
+  const options = {
+    method: 'GET',
+    credentials: 'include',
+  };
+  await fetch(`http://${backendIPAddress}/courseville/get_courses`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('ces-cid-value').innerHTML = data.data.student.filter(
+        (course) => course.course_no == '2110221'
+      )[0].cv_cid;
+    })
+    .catch((error) => console.error(error));
 };
 
 // TODO #3.5: Send Get Course Assignments ("GET") request with cv_cid to backend server
 //            and create Comp Eng Ess assignments table based on the response (itemid, title)
 const createCompEngEssAssignmentTable = async () => {
-  const table_body = document.getElementById("main-table-body");
-  table_body.innerHTML = "";
-  const cv_cid = document.getElementById("ces-cid-value").innerHTML;
+  const table_body = document.getElementById('main-table-body');
+  table_body.innerHTML = '';
+  const cv_cid = document.getElementById('ces-cid-value').innerHTML;
 
-  console.log(
-    "This function should fetch 'get course assignments' route from backend server and show assignments in the table."
-  );
+  const options = {
+    method: 'GET',
+    credentials: 'include',
+  };
+  await fetch(`http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      data.data.map((assignment) => {
+        table_body.innerHTML += `
+        <td>${assignment.itemid}</td>
+        <td>${assignment.title}</td>
+        `;
+      });
+    })
+    .catch((error) => console.error(error));
 };
 
 const logout = async () => {
   window.location.href = `http://${backendIPAddress}/courseville/logout`;
 };
 
-document.getElementById("group-id").innerHTML = getGroupNumber();
+document.getElementById('group-id').innerHTML = getGroupNumber();
